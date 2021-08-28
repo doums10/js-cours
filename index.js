e2f236be39bee5b2016cdd05b16c7ff7cf156aea
doums10 <m27sylla@gmail.com>
2021-08-27 20:18:54 -0400

    variables done

---
+++ index.js
@@ -0,0 +1,13 @@
+//VARIABLES
+let cours = "qsdsdl kfjkfjkd";
+console.log(cours + ' //Avant MAJ');
+
+
+cours = "hhttjjjif";
+console.log(cours + ' // Après MAJ');
+
+//functions
+
+function faireQuelqueChose() {
+	console.log('je fais un truc');
+}
