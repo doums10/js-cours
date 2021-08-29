4dbc6ce5629366f6f74f131d6bfb4493e6ae929f
doums10 <m27sylla@gmail.com>
2021-08-29 17:18:12 -0400

    events avec switch done

--- events/index.js
+++ events/index.js
@@ -25,3 +25,35 @@ mouseEvent.addEventListener("mousemove", (e) => {
 document.getElementById("input").addEventListener("input", (e) => {
   vertical.innerHTML = e.target.value;
 });
+
+// **************************************
+const theme = document.querySelectorAll(".theme");
+const borderDark = document.querySelector("#dark");
+const borderSalmon = document.querySelector("#salmon");
+const borderYellow = document.querySelector("#yellow");
+
+theme.forEach((item) => {
+  item.addEventListener("click", (e) => {
+			document.body.classList.remove("darkTheme","yellowTheme", "salmonTheme");
+			borderDark.classList.remove("borderDark");
+			borderSalmon.classList.remove("borderSalmon");
+			borderYellow.classList.remove("borderYellow");
+    switch (e.target.id) {
+      case "dark":
+        document.body.classList.add("darkTheme");
+								borderDark.classList.add("borderDark");
+        break;
+      case "salmon":
+        document.body.classList.add("salmonTheme");
+								borderSalmon.classList.add("borderSalmon");
+        break;
+      case "yellow":
+        document.body.classList.add("yellowTheme");
+								borderYellow.classList.add("borderYellow");
+        break;
+      default:
+        null;
+        break;
+    }
+  });
+});
