b2cef1976906c719c816dac8412bcabd4a515c1d
doums10 <m27sylla@gmail.com>
2021-09-06 10:41:39 -0400

    rappel sur les types de données et petit exemple sur les array

---
+++ cours-P3/index.js
@@ -0,0 +1,25 @@
+// Rappel sur les types de données
+let string = "Chaine";
+let number = 25;
+let boolean = true;
+let maVariable; // type undefined
+
+// Tableaux
+let array = ["Bordeaux", "Nantes", "Paris"];
+//console.log(array[1][2]);
+
+let array2 = ["Bordeaux", 10, [3, 4], false, { nom: "Denis" }];
+//console.log(array2[4].nom);
+let object = {
+  name: "Denis",
+  age: 33,
+  technos: ["React", "Javascript", "NodeJs"],
+  admin: false,
+};
+// Dans un object 
+// ex admin:false
+// admin = index false = valeur
+
+//ajouter un élément dans un objet
+//object.adresse = "22 rue du code";
+// console.log(object.adresse);
